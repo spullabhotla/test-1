f85657adf8976c229385f535280683f15da1b393
Sai Pullabhotla <sai.pullabhotla@gmail.com>
2016-06-30 17:50:08 -0500

    Fixed the POM

--- src/main/java/com/jmethods/test/App.java
+++ src/main/java/com/jmethods/test/App.java
@@ -1,11 +1,11 @@
 package com.jmethods.test;
 
 /**
- * Hello world!
+ * Hello world v0.04!
  *
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World v0.04!");
 	}
 }
