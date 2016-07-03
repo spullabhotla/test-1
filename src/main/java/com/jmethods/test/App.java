179247d74896d57df2f2ef38d26861869b227cfe
Sai Pullabhotla <sai.pullabhotla@gmail.com>
2016-07-03 17:11:04 -0500

    Changed the version to 0.06

--- src/main/java/com/jmethods/test/App.java
+++ src/main/java/com/jmethods/test/App.java
@@ -6,6 +6,6 @@ package com.jmethods.test;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World v0.04!");
+		System.out.println("Hello World v0.06!");
 	}
 }
